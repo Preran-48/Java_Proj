e43ee4b472dfd8d4b98b2cd0de358a8548335429
Preran-48 <prerankumar.n@qspiders.in>
2026-02-23 12:55:49 +0530

    Remove duplicate welcome message
    Removed duplicate print statement in main method.

--- Sample.java
+++ Sample.java
@@ -3,9 +3,10 @@ class Sample
     public static void main (String[] args)
     {
      System.out.println("Welcome to DevOps");
-     System.out.println("Welcome to DevOps");
+     
     }
   }
+
 
 
 
