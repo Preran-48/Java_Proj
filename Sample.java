0b183cda000d0caec2bd12f00ad026ac2f436f4b
Preran-48 <prerankumar.n@qspiders.in>
2026-08-28 12:36:51 +0530

    Add Jenkins polling message to Sample.java

--- Sample.java
+++ Sample.java
@@ -3,6 +3,7 @@ class Sample
     public static void main (String[] args)
     {
      System.out.println("Welcome to DevOps");
+     System.out.println("configuring jenkins poll SCM");
     }
   }
 
